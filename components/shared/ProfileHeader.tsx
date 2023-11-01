50aebe5c94ff8ec8c5c38ecd3a65cc8f8cc9b360
Yongjae Kim <mearak43@gmail.com>
2023-11-02 06:09:55 +0900

    feat: profile page done

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -29,8 +29,17 @@ const ProfileHeader = ({
               className="rounded-full object-cover shadow-2xl"
             />
           </div>
+          <div className="flex-1">
+            <h2 className="text-left text-heading3-bold text-light-1">
+              {name}
+              <p className="text-base-medium text-gray-1">@{username}</p>
+            </h2>
+          </div>
         </div>
+        {/* todo community */}
       </div>
+      <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
+      <div className="mt-12 h-0.5 w-full bg-dark-3" />
     </div>
   );
 };
